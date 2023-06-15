a5a652453132fa76db6acaac968dea85380950d5
JohanCode2180100 <Johan_CODE21@orange.fr>
2023-06-15 16:34:14 +0200

    add a httpClient Service and Angular API

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,17 +1,30 @@
 import { NgModule } from "@angular/core";
 import { BrowserModule } from "@angular/platform-browser";
+import { HttpClientModule } from "@angular/common/http";
 import { AppComponent } from "./app.component";
 import { AppRoutingModule } from "./app-routing.module";
 import { PageNotFoundComponent } from "./page-not-found/page-not-found.component";
 import { PokemonModule } from "./pokemon/pokemon.module";
 import { FormsModule } from "@angular/forms";
+import { HttpClientInMemoryWebApiModule } from "angular-in-memory-web-api";
+//import du service API cree
+import { InMemoryDataService } from "./in-memory-data.service";
 
 @NgModule({
   //declarations des vues
   declarations: [AppComponent, PageNotFoundComponent],
   //classes necessaires au fonctionnement du module
   //ATTENTION A LA DECLARATION DES FICHIERS - POKEMON MODULE AVANT APP ROUTINGMODULE
-  imports: [BrowserModule, FormsModule, PokemonModule, AppRoutingModule],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    HttpClientInMemoryWebApiModule.forRoot(InMemoryDataService, {
+      //false pour ne pas encapsuler nos elements de reponses dans un element DATA
+      dataEncapsulation: false,
+    }),
+    PokemonModule,
+    AppRoutingModule,
+  ],
   //permet de fournir un service
   providers: [],
   //le composant racine pour le module racine, bootstrap de l app
--- src/app/pokemon/pokemon.service.ts
+++ src/app/pokemon/pokemon.service.ts
@@ -1,18 +1,37 @@
 import { Injectable } from "@angular/core";
 import { Pokemon } from "./pokemon";
-import { POKEMONS } from "./mock-pokemon-list";
+import { HttpClient } from "@angular/common/http";
+import { Observable, catchError, of, tap } from "rxjs";
 
 //mecanisme d injection des dependances
 @Injectable()
 //utilisation dans toute l app si 'root'
 //supprime si injection dans une partie de l app, voir pokemon.module => providers:[pokemonService]
 export class PokemonService {
-  getPokemonList(): Pokemon[] {
-    return POKEMONS;
+  //httpClient injection de dependances
+  constructor(private http: HttpClient) {}
+
+  getPokemonList(): Observable<Pokemon[]> {
+    return this.http.get<Pokemon[]>("api/pokemons").pipe(
+      //tap = clg.log adpate a un observable
+      tap((pokemonList) => console.table(pokemonList)),
+      //intercepter les erreurs
+      catchError((error) => {
+        console.log(error);
+        return of([]);
+      })
+    );
   }
 
-  getPokemonByID(pokemonId: number): Pokemon | undefined {
-    return POKEMONS.find((pokemon) => pokemon.id == pokemonId);
+  getPokemonByID(pokemonId: number): Observable<Pokemon | undefined> {
+    return this.http.get<Pokemon>(`api/pokemons/${pokemonId}`).pipe(
+      tap((pokemon) => console.table(pokemon)),
+      //intercepter les erreurs
+      catchError((error) => {
+        console.log(error);
+        return of(undefined);
+      })
+    );
   }
 
   getPokemonTypeList(): string[] {
